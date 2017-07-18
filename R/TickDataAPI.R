a8c1744a26462f23e894fe27dd31672b7db1a9a8
VeliKupeli <velican.kupeli@matriksdata.com>
2017-07-18 11:04:33 +0300

    cat correction

--- R/TickDataAPI.R
+++ R/TickDataAPI.R
@@ -67,7 +67,7 @@ getData<-function(dateRange,symbol,type,period=NULL,isLocal=NULL){
 
   counter <- 1
   while(T){
-    cat(mainurl)
+    cat(mainurl, "\n")
     req <- tryCatch(GET(mainurl, add_headers(Authorize = paste("jwt",auth))), error = function(e) {cat("Error\n");next})
     # cat(req$status_code,"\n")
     if(req$status_code == 200){
